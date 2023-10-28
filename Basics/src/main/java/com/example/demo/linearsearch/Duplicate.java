2a2b544741e6d5e32cd3175ae82c5adc34d40d11
Abu <abuabakrsiddique.d@gmail.com>
2023-10-28 21:33:44 +0530

    Adding a Program to find duplicate elements in an array

---
+++ Basics/src/main/java/com/example/demo/linearsearch/Duplicate.java
@@ -0,0 +1,37 @@
+package com.example.demo.linearsearch;
+
+import java.util.HashSet;
+
+public class Duplicate {
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,1};
+        System.out.println(containsDuplicateHashSet(arr));
+    }
+
+    //O(log(n))
+    public static boolean containsDuplicateBruteForce(int[] nums) {
+        int[] fr = new int[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return  true;
+                }
+            }
+        }
+        return  false;
+    }
+
+    //O(n)
+    public static boolean containsDuplicateHashSet(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
